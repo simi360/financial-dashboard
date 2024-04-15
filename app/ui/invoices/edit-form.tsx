b84c8f8438b889bd6643aadb1cc9488224a88751
Harsimran Kaur <harsimrankaur0360@gmail.com>
2024-04-14 20:04:51 -0400

    added: update and delete for invoices

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { updateInvoice } from '@/app/lib/actions';
 import { CustomerField, InvoiceForm } from '@/app/lib/definitions';
 import {
   CheckIcon,
@@ -17,8 +18,9 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
+  const updateInvoiceById = updateInvoice.bind(null, invoice.id);
   return (
-    <form>
+    <form action={updateInvoiceById}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
