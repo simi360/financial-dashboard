4b0b7be5868a18c1ec288293b11476e711333283
Harsimran Kaur <harsimrankaur0360@gmail.com>
2024-04-21 20:02:03 -0400

    Created customer's skeleton views

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,25 @@
 import { Metadata } from "next";
+import Search from "@/app/ui/search";
+import { lusitana } from "@/app/ui/fonts";
+import { CreateCustomer } from "@/app/ui/customers/buttons";
+import { CustomerTableSkeleton } from "@/app/ui/skeletons";
 
 export const metadata: Metadata = {
     title: 'Customers'
 }
 
 export default function Customers() {
-    return <p>Customers page</p>
+
+    return (
+        <div className="w-full">
+            <div className="flex w-full items-center justify-between">
+                <h1 className={`${lusitana.className} text-2xl`}>Customers</h1>
+            </div>
+            <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+                <Search placeholder="Search Customers..."/>
+                <CreateCustomer />
+            </div>
+            <CustomerTableSkeleton />
+        </div>
+    )
 }
